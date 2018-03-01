150f40ffe7c6ae96e3caf9a326c32f681e580c6c
Sneha Dudaki <sneha.dudaki@orionhealth.com>
2018-03-01 13:58:16 +1300

    My very first commit!

---
+++ src/Test/TestGitHub.java
@@ -0,0 +1,21 @@
+package Test;
+/**
+ * 
+ */
+
+/**
+ * @author snehad
+ *
+ */
+public class TestGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yayyyy");
+
+	}
+
+}
